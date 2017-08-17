cc0a0aa0b01221da05cc00b474b9b88cf2fcf3ac
Sergio Martinez <dialstudios@gmail.com>
2017-08-16 21:03:32 -0500

    add  maqueta part 50%

--- src/js/custom.js
+++ src/js/custom.js
@@ -47,4 +47,39 @@ $(function(){
           }
         }]
     });
+    // slider slick home
+    $('.promo-item').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 950,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }]
+    });
 });

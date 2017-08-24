cf4ec7e90605eabd2a31c3b6ab9863a21b392b1a
Sergio Martinez <dialstudios@gmail.com>
2017-08-23 22:01:33 -0500

    add ajustes responsive

--- src/js/custom.js
+++ src/js/custom.js
@@ -161,7 +161,9 @@ $(function(){
           breakpoint: 768,
           settings: {
             slidesToShow: 1,
-            slidesToScroll: 1
+            slidesToScroll: 1,
+            infinite: true,
+            arrows: true
           }
         }]
     });

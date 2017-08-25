6107d613597be27ae684df05be2539d6c594e64b
Sergio Martinez <dialstudios@gmail.com>
2017-08-24 21:55:37 -0500

    add site create site

--- src/js/custom.js
+++ src/js/custom.js
@@ -189,4 +189,86 @@ $(function(){
             //console.log('on');
         }
     });
+    // formumlario
+    $('.alerta').slideUp();
+    $('.envio').click(function() {
+        /* Act on the event */
+        var type = $('#type option:selected').text();
+        var fullName = $('#fullName').val();
+        var business = $('#business').val();
+        var city = $('#city').val();
+        var phone = $('#phone').val();
+        var email = $('#email').val();
+        var validacion_email = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+        var message = $('#message').val();
+        if(type == 'Tipo' || fullName == '' || business == '' || city == '' || phone == '' || email == '' || message == ''){
+            if(type == 'Tipo'){
+                $('.select').addClass('error');
+                $('#type').focus();
+                return false;
+            }else{
+                $('.select').removeClass('error');
+            }if (fullName == ''){
+                $('.fullName').addClass('error');
+                $('#fullName').focus();
+                return false;
+            }else{
+                $('.fullName').removeClass('error');
+            }if (business == ''){
+                $('.business').addClass('error');
+                $('#business').focus();
+                return false;
+            }else{
+                $('.business').removeClass('error');
+            }if(city == ''){
+                $('.city').addClass('error');
+                $('#city').focus();
+                return false;
+            }else{
+                $('.city').removeClass('error');
+            }if(phone == ''){
+                $('.phone').addClass('error');
+                $('#phone').focus();
+                return false;
+            }else{
+                $('.phone').removeClass('error');
+            }if(email == '' || !validacion_email.test(email)){
+                $('.email').addClass('error');
+                $('#email').focus();
+                return false;
+            }else{
+                $('.email').removeClass('error');
+            }if(message == ''){
+                $('.message').addClass('error');
+                $('#message').focus();
+                return false;
+            }else{
+                $('.message').removeClass('error');
+            }
+        }else{
+            $('.message').removeClass('error');
+            var datos = '&type=' + type + '&fullName=' + fullName + '&business=' + business + '&city=' + city + '&phone=' + phone + '&email=' + email + '&message=' + message ;
+            $.ajax({
+                method: 'Post',
+                url:'envio.php',
+                dataType: 'json',
+                data:datos,
+            }).done(function(msg){
+                if(msg.success){
+                    $('.alerta').slideDown('slow');
+                    $('.alerta p').html('Gracias, pronto nos contactaremos.');
+                    $('#type option:selected').text();
+                    $('#fullName').val('');
+                    $('#business').val('');
+                    $('#city').val('');
+                    $('#phone').val('');
+                    $('#email').val('');
+                }else{
+                    $('.alerta').slideDown('slow');
+                    $('.alerta p').html('Error, Intente luego.');
+                }
+            });
+            return false;
+        }
+    });
 });
